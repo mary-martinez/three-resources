2a03244fbe2410509ad8f234ae6da11b36817111
Mary Martinez <mary.diana.martinez@gmail.com>
2022-03-22 15:45:41 -0700

    reconfigured to use router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './view/Main/Main';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
+import ToysView from './view/Toys/ToysView';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Header
-      </header>
-      <Main />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route path="/toys">
+            <ToysView />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/view/Main/Main.js
+++ src/view/Main/Main.js
@@ -1,24 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import Toys from '../../components/Toys/Toys';
-import { getToys } from '../../services/toys';
+
 
 export default function Main() {
-  const [toys, setToys] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const resp = await getToys();
-      setToys(resp);
-    };
-    fetchData();
-  }, []);
 
   return (
     <div>
-      {toys.map((toy) => (
-        <Toys key={(toy.id)} colors={toy.colors} name={toy.name} strength={toy.strength} type={toy.type} />
-
-      ))}
+      <h1>Random Assortment of Project Info</h1>
     </div>
   );
 }
