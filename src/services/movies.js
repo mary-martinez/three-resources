a0efe31e2943898f062609379103acfdbcb9638d
Mary Martinez <mary.diana.martinez@gmail.com>
2022-03-23 09:08:15 -0700

    cleaning up code

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,6 +1,5 @@
 import { client, checkError } from './client.js';
 export async function fetchMovies(query = '') {
-  console.log(query);
   let resp;
   if (query.length > 0) {
     resp = await client.from('movies').select('*').ilike('title', `%${query}%`);
