91d8283c10c4a27de367538dc490f9ae8c35d232
Mary Martinez <mary.diana.martinez@gmail.com>
2022-03-22 17:47:00 -0700

    search function working

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,5 +1,12 @@
 import { client, checkError } from './client.js';
-export async function fetchMovies() {
-  const resp = await client.from('movies').select('*');
+export async function fetchMovies(query = '') {
+  console.log(query);
+  let resp;
+  if (query.length > 0) {
+    resp = await client.from('movies').select('*').ilike('title', `%${query}%`);
+
+  } else {
+    resp = await client.from('movies').select('*');
+  }
   return (checkError(resp));
 }
